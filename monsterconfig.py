bc704ef1e608e121c807b33db43f7ce3b877fbca
deconf <deconf@ya.ru>
2023-07-15 07:29:06 +0300

    sdf

--- monsterconfig.py
+++
@@ -1,7 +0,0 @@
-API_KEY = '5jbyNSSpNV3rIcnXM6jpg8m9IZe33XbVWmwAgI8i'
-API_BEARER = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTE0OTE5OTUsImlhdCI6MTY4ODg5OTk5NSwic3ViIjo" \
-             "iNzA1MTUzOTczYzZjYjg0NTlmYjRlODg2YjNmMjcyMTQifQ.MQ8ubkvk58S39wyg26sQ-CHtbuu4_Y-xVgKHe2TUG4s"
-PROMPT = "2 new axises, 3 wires out from black hole near corner of universe, 1 axis near"
-NEGPROMPT = "collage, tables, rows, columns, lowres, worst quality, low quality, " \
-            "jpeg artifacts, bad quality, memes, body horror, doll like, doll, charts"
-dd = 4
